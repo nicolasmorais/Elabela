7ccdbe9239a4e4d8c9c934c8924a098a69163da5
[dyad] <nicolasmorais154@gmail.com>
2025-12-06 16:59:20 -0300

    [dyad] Restaurar rodapé detalhado da página de aprovação - wrote 3 file(s)

--- src/components/advertorial-ap/FooterAP.tsx
+++ src/components/advertorial-ap/FooterAP.tsx
@@ -10,7 +10,7 @@ export const FooterAP = ({ disclaimer, copyright }: FooterAPProps) => {
     <footer className="bg-gray-800 text-gray-300">
       <div className="bg-gray-900 py-6">
         <div className="max-w-4xl mx-auto px-4 text-center text-xs text-gray-400 space-y-3">
-          <p>{disclaimer}</p>
+          <p dangerouslySetInnerHTML={{ __html: disclaimer }} />
           <p>{copyright}</p>
         </div>
       </div>
