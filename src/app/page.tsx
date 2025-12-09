1a7989fffd48f10cac7ca4c298db7a8261efb2da
[dyad] <nicolasmorais154@gmail.com>
2025-12-08 23:13:32 -0300

    [dyad] Removed conflicting page.tsx file to fix route specificity error - deleted 1 file(s)

--- src/app/page.tsx
+++
@@ -1,6 +0,0 @@
-import { redirect } from 'next/navigation';
-
-export default function RootPage() {
-  // Redirect to the dynamic route handler which will determine the correct content
-  redirect('/');
-}
