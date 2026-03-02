f335fce2595d0ecc27cb893c9302b986fc5aab15
[dyad] <nicolasmorais154@gmail.com>
2026-03-02 00:13:04 -0300

    Updated src/components/page-versions/AntiHairLossPageV2.tsx

--- src/components/page-versions/AntiHairLossPageV2.tsx
+++ src/components/page-versions/AntiHairLossPageV2.tsx
@@ -61,7 +61,7 @@ import { MobileStickyBar } from './MobileStickyBar';
 
 // IMAGENS EXCLUSIVAS DA GALERIA DE PRODUTO (TOPO)
 const PRODUCT_IMAGES = [
-  "https://pub-da9fd1c19b8e45d691d67626b9a7ba6d.r2.dev/1772421083925-image-(2)-(1).jpg",
+  "https://pub-da9fd1c19b8e45d691d67626b9a7ba6d.r2.dev/1772416572105-unnamed-(5)-(1).jpg",
   "https://pub-da9fd1c19b8e45d691d67626b9a7ba6d.r2.dev/1772416572105-unnamed-(5)-(1).jpg",
   "https://pub-da9fd1c19b8e45d691d67626b9a7ba6d.r2.dev/1770558652832-5.png"
 ];
